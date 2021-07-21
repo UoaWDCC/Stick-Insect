738b21beb5017ec981188aced6b027f016b565d8
jekb2019 <jekb2019@gmail.com>
2021-07-21 22:26:30 +1200

    [STINS-005] Change CSS file into module
    Issue: CSS class names can confilct among components when module css is not used.
    Solution: Change CSS file into module - from "ConsentPage.css" to "ConsentPage.module.css".
    Risk: No risk.

--- frontend/src/components/ConsentPage.js
+++ frontend/src/components/ConsentPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './ConsentPage.css';
+import styles from './ConsentPage.module.css';
 
 const ConsentPage = (props) => (
-        <section className="consent-page-wrapper">
-            <header className="consent-page-header">
+        <section className={styles.consent_page_wrapper}>
+            <header className={styles.consent_page_header}>
                 <h1>Participant Information and Consent Form</h1>
             </header>
-            <div className="consent-page-agreement">
+            <div className={styles.consent_page_agreement}>
                 <p>
                     By clicking agree below... lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                 </p>
                 
             </div>
-            <div className="consent-page-agreement-btns">
+            <div className={styles.consent_page_agreement_btns}>
                 <button>I AGREE</button>
                 <button>NO, TAKE ME AWAY</button>
             </div>
