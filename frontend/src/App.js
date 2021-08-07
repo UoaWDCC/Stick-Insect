a946c2977fe30959f72c82a7f7aff839dc0a17f0
Aidan Dayvyd <adayvyd@gmail.com>
2021-08-07 18:09:35 +1200

    Issue: Create a prototype for the third "played before?" page. 	Solution: Followed the figma mockup.  Was very simple because I could re-use the frontend code from the first sprint, with minor tweaks. 	Risk: I don't think this will affect other peoples' work thus far, but it may cause trouble in button/page implementation if I haven't done that correctly.           I'm also worried about committing package-lock.json because it caused issues in my last task.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
   
 import logo from './logo.svg';
 import './App.css';
-import ConsentPage from './components/ConsentPage';
+import QuestionPage from './components/QuestionPage';
+
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <ConsentPage/>
+      <QuestionPage/>
     </div>
   );
 }
