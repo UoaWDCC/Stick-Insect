f5e36191f6201a0a95bf6f947e55ae279f0dd608
jzha411 <jzha411@aucklanduni.ac.nz>
2021-08-07 14:13:57 +1200

    [STINS-003] Implement the main page using proto design
    Issue: Unnecessary lines within App.js
    Solution: Deleted said lines
    Risk: There should be no risk
    Reviewed by: Jason

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-/*import logo from './logo.svg';*/
 import './App.css';
 import './components/MainPage'
 import MainPage from './components/MainPage';
@@ -9,20 +8,6 @@ function App() {
       <header classname = "App-header">
         <MainPage/>
       </header>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
