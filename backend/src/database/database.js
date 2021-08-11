0fd1f3b0dfb8501be53c571a8bac518a323f52dd
Jason Ko <jekb2019@gmail.com>
2021-08-11 15:31:03 +1200

    [STINS-008] connect-backend-with-mongodb-atlas Issue: Connect server with mongoDB atlas Solution: Use Mongoose to easily connect to Atlas Risk: No risk. Reviewed by: Alex, Aiden

---
+++ backend/src/database/database.js
@@ -0,0 +1,20 @@
+import Mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env["DB_HOST"];
+
+// This function connects our server to MongDB Atlas
+export async function connectDB() {
+  Mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = Mongoose.connection;
+
+  db.on("error", () => console.error("Error occured"));
+  db.once("open", () => {
+    console.log("DB Successfully Connected!");
+  });
+}
