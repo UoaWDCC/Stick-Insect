325de8b1238c5cd4b4f135ed4cd39e43634e577d
Jason Ko <jekb2019@gmail.com>
2021-08-20 18:40:32 +1200

    Toss to Hard Jason
    Issue: Code Review for Jason
    Solution: Toss it to hard Jason
    Risk: No risk
    Reviewed by: Hard Jason

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,27 @@ import QuestionPage from './components/QuestionPage';
 //Parent class for the page components
 function App() {
   //Declares new setPage variable, which we call "page"
+  const PAGE_MAIN = "main";
+  const PAGE_CONSENT = "consent";
+  const PAGE_QUESTION = "question";
+
   const [page, setPage] = React.useState("");
   function pageChange(newPage) {
-    setPage(newPage);
+    if(newPage === PAGE_MAIN) {
+      setPage(PAGE_MAIM);
+    } else if(newPage === PAGE_CONSENT) {
+      setPage(PAGE_CONSENT);
+    } else if(newPage === PAGE_QUESTION) {
+      setPage(PAGE_QUESTION);
+    }
   }
+
+
+
   // We pass a callback to Child
-  return <Pagebuttonpress page = {page} onButtonClick ={pageChange}>
-    
-
-
-  </Pagebuttonpress>;
+  return (
+    <Pagebuttonpress page = {page} onButtonClick ={pageChange}/>
+  )
 }
 
 {['MainPage', 'ConsentPage', 'QuestionPage'].map((page) => (
