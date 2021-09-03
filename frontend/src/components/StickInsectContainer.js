c0d493a6bb7423cb9a1cf4ff3b1e6e08e99584f4
Joel Hutchinson <joelkahuhutch@gmail.com>
2021-09-04 03:15:32 +1200

    [STINS-009] render a stick insect on the webpage

--- frontend/src/components/StickInsectContainer.js
+++ frontend/src/components/StickInsectContainer.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react'
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const StyledStickInsect = styled.div`
-    & {
-        position: absolute;
-        top: ${Math.floor(Math.random()*80)+10}%;
-        left: ${Math.floor(Math.random()*80)+10}%;
-        color: #FFFFFF;
-        padding: 1rem;
-        transform: rotate(${Math.floor(Math.random()*180)}deg);
-        background-color: #964B00
-    
-    }
-    `
-    
-    const StyledInsectContainer = styled.div`
+  & {
+    position: absolute;
+    top: ${(props) => props.topPos || Math.floor(Math.random() * 80) + 10}%;
+    left: ${(props) => props.leftPos || Math.floor(Math.random() * 80) + 10}%;
+    color: #ffffff;
+    padding: 1rem;
+    transform: rotate(${Math.floor(Math.random() * 180)}deg);
+    background-color: #964b00;
+  }
+`;
+
+const StyledInsectContainer = styled.div`
         & {
             position: relative;
             width: 100%
             height: 100%
         }
-    `
+    `;
 
-export class StickInsectContainer extends Component {
+export function StickInsectContainer(props) {
+  const [randLeftPos, setLeftPos] = useState("");
+  const [randTopPos, setTopPos] = useState("");
 
-    
-    render() {
-        return (
-            <StyledInsectContainer>
-                <StyledStickInsect onClick={() => alert("Found me!")}>Stick Insect</StyledStickInsect>
-            </StyledInsectContainer>
-        )
-    }
+  const updateInsect = () => {
+    alert("Found me!");
+    setLeftPos(Math.floor(Math.random() * 80) + 10);
+    setTopPos(Math.floor(Math.random() * 80) + 10);
+  };
+
+  return (
+    <StyledInsectContainer>
+      <StyledStickInsect
+        leftPos={randLeftPos}
+        topPos={randTopPos}
+        onClick={updateInsect}
+      >
+        Stick Insect
+      </StyledStickInsect>
+    </StyledInsectContainer>
+  );
 }
 
-export default StickInsectContainer
+export default StickInsectContainer;
