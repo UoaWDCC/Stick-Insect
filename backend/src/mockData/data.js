b1558d55372670ed0dcb8f45f217027315409d09
dlee658 <55651714+dlee658@users.noreply.github.com>
2021-09-17 19:44:39 +1200

    added mock data

--- backend/src/mockData/data.js
+++ backend/src/mockData/data.js
@@ -1,73 +1,142 @@
 /**
  * Below is the mock data for games
  */
-const game1 = {
-  rounds: [
-    {
-      roundNum: 1,
-      backgroundId: "bg1",
-      timeTaken: 10,
-      isFound: true,
-    },
-    {
-      roundNum: 2,
-      backgroundId: "bg2",
-      timeTaken: 15,
-      isFound: true,
-    },
-    {
-      roundNum: 3,
-      backgroundId: "bg3",
-      timeTaken: 15,
-      isFound: false,
-    },
-  ],
-};
+const MockData = () => {
+  const game1 = {
+    rounds: [
+      {
+        roundNum: 1,
+        backgroundId: "bg1",
+        timeTaken: 10,
+        isFound: true,
+      },
+      {
+        roundNum: 2,
+        backgroundId: "bg2",
+        timeTaken: 15,
+        isFound: true,
+      },
+      {
+        roundNum: 3,
+        backgroundId: "bg3",
+        timeTaken: 15,
+        isFound: false,
+      },
+    ],
+    isFirstAttempt: true,
+  };
 
-const game2 = {
-  rounds: [
-    {
-      roundNum: 1,
-      backgroundId: "bg3",
-      timeTaken: 3,
-      isFound: true,
-    },
-    {
-      roundNum: 2,
-      backgroundId: "bg2",
-      timeTaken: 6,
-      isFound: true,
-    },
-    {
-      roundNum: 3,
-      backgroundId: "bg1",
-      timeTaken: 6,
-      isFound: true,
-    },
-  ],
-};
+  const game2 = {
+    rounds: [
+      {
+        roundNum: 1,
+        backgroundId: "bg3",
+        timeTaken: 3,
+        isFound: true,
+      },
+      {
+        roundNum: 2,
+        backgroundId: "bg2",
+        timeTaken: 6,
+        isFound: true,
+      },
+      {
+        roundNum: 3,
+        backgroundId: "bg1",
+        timeTaken: 6,
+        isFound: true,
+      },
+    ],
+    isFirstAttempt: true,
+  };
 
-const game3 = {
-  rounds: [
-    {
-      roundNum: 1,
-      backgroundId: "bg2",
-      timeTaken: 13,
-      isFound: true,
-    },
-    {
-      roundNum: 2,
-      backgroundId: "bg2",
-      timeTaken: 15,
-      isFound: true,
-    },
-    {
-      roundNum: 3,
-      backgroundId: "bg1",
-      timeTaken: 15,
-      isFound: false,
-    },
-  ],
-};
+  const game3 = {
+    rounds: [
+      {
+        roundNum: 1,
+        backgroundId: "bg2",
+        timeTaken: 13,
+        isFound: true,
+      },
+      {
+        roundNum: 2,
+        backgroundId: "bg2",
+        timeTaken: 15,
+        isFound: true,
+      },
+      {
+        roundNum: 3,
+        backgroundId: "bg1",
+        timeTaken: 15,
+        isFound: false,
+      },
+    ],
+    isFirstAttempt: true,
+  };
 
-const dataset = [game1, game2, game3];
+  const game4 = {
+    rounds: [
+      {
+        roundNum: 1,
+        backgroundId: "bg2",
+        timeTaken: 13,
+        isFound: true,
+      },
+      {
+        roundNum: 2,
+        backgroundId: "bg1",
+        timeTaken: 7,
+        isFound: true,
+      },
+      {
+        roundNum: 3,
+        backgroundId: "bg1",
+        timeTaken: 9,
+        isFound: true,
+      },
+    ],
+    isFirstAttempt: false,
+  };
+
+  const game5 = {
+    rounds: [
+      {
+        roundNum: 1,
+        backgroundId: "bg2",
+        timeTaken: 3,
+        isFound: true,
+      },
+      {
+        roundNum: 2,
+        backgroundId: "bg2",
+        timeTaken: 5,
+        isFound: true,
+      },
+      {
+        roundNum: 3,
+        backgroundId: "bg1",
+        timeTaken: 15,
+        isFound: false,
+      },
+    ],
+    isFirstAttempt: false,
+  };
+
+  const email1 = {
+    email: 'test@test.com',
+  }
+  
+  const email2 = {
+    email: 'emailt@test.com',
+  }
+  
+  const email3 = {
+    email: 'email2@test.com',
+  }
+
+  const email4 = {
+    email: "user123@example.com",
+  }
+}
+
+export default MockData;
