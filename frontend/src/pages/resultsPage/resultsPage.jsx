d915a84d7d5cfc01cb93c655ac9b26e60892c326
dhani99 <42270532+dhani99@users.noreply.github.com>
2021-11-26 19:45:51 +1300

    [STINS-038] implemented-results-page
    Issue: We did not have a result page before so we were just replacing the place holder with the content that needed to be on the result page.
    
    Solution: Uploaded the content that was needed on the result page
    Risk: yes, it will help the people implementing the email input section on that page.
    
    Reviewed by: Jason Ko, Jason Zhao, Aidan Dayvyd

--- frontend/src/pages/resultsPage/resultsPage.jsx
+++ frontend/src/pages/resultsPage/resultsPage.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {useState, useRef} from "react";
+import Results from "../../components /results /results";
+import styles from "./mainPage.module.css";
 
-const ResultsPage = ({ isPlayedBefore, gameResults }) => (
-  <div>
-    <h1>Results</h1>
-    <p>Played before: {isPlayedBefore}</p>
-    <p>Game Results: {JSON.stringify(gameResults)}</p>
-  </div>
-);
+
+const MainPage = (props) => {
+    
+   
+    return(
+    <section className = {styles.whole_wrapper}>
+    <div>
+        <h1 className = {styles.title}>Spot the Stick Insect</h1>
+        <div>
+        <h4>Thank you for participating!</h4>
+        <h5>You spotted 18 stick instects out of 20,<br /> in an average time of 9.68 seconds!</h5>
+            
+        </div>
+        <div>
+            <p>If you would like to be notified of the outcome of this research, please leave <br /> your email address below. If you choose to supply your email address, <br />we will not for any other purpose, nor will we supply it to any third party. </p>
+            
+        </div>
+        <div>
+            <label> Email: </label>
+            <input type="text" />
+            <button className={styles.email_btn} > Submit</button>
+            
+        </div>
+        <br/>
+        <div className = {styles.button_wrapper}>
+           
+            <button className={styles.play_btn} > Try Again! </button>
+
+        </div>
+    </div>
+    </section>
+    );
+
+}
 
 export default ResultsPage;
