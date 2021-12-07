5d1bbdd423b9a579ed6769dd11d08397abd9e00a
Joel <64521958+butterplup@users.noreply.github.com>
2021-12-08 00:02:01 +1300

    [STINS-039] img-path-passed-as-prop (#35)
    Issue:
    Background and Stick Insect images were hardcoded so could not change dynamically.
    
    Solution:
    An index is passed in as a prop which selects which image is displayed.
    
    Risk:
    The new prop must be input into the component as no default is set. Slight CSS changes may mess with the parent Game Page. Image filenames must follow the current convention else won't work.
    
    Reviewed By:
    Jason, Aidan

--- frontend/src/components/gameBoard/gameBoard.jsx
+++ frontend/src/components/gameBoard/gameBoard.jsx
@@ -2,18 +2,40 @@ import React from "react";
 import styles from "./gameBoard.module.css";
 import StickInsectContainer from "../stickInsectsContainer/stickInsectContainer";
 
+// Function imports all images from a file location
+function importAllImages(r) {
+  const images = {};
+  r.keys().forEach((item) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+// An array containing all the images imported from the backgrounds folder
+const backgroundImages = importAllImages(
+  require.context(
+    "../../assets/images/backgrounds",
+    false,
+    /\.(png|jpe?g|svg)$/
+  )
+);
+
 /**
  * This component is the Game Board a child component of the Game Page.
  *
  * receiving props:
- *   - TBD
- *   - Background Image filename
- *   - Removed for es-lint until can be used
+ *   - Index, the number of the background image in the folder
  */
-function GameBoard() {
+function GameBoard(props) {
+  const { currentRound } = props;
+
   return (
     <div className={styles.background_wrapper}>
-      <StickInsectContainer />
+      <img
+        src={backgroundImages[`B${currentRound}-TH1c.png`].default}
+        alt="Natural Scenery"
+        className={styles.image_styles}
+      />
+      <StickInsectContainer index="1" className={styles.stick_styles} />
     </div>
   );
 }
