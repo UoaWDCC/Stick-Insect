fc08e50168c53627a2afb29685007b6d56df3ebe
dhani99 <42270532+dhani99@users.noreply.github.com>
2021-12-07 22:51:09 +1300

    [STINS-038] implemented-results-page (#34)
    * [STINS-038] implemented-results-page
    
    Issue: We did not have a result page before so we were just replacing the placeholder with the content that needed to be on the result page.
    Solution: Created the blank content that was needed on the result page
    Risk: May interact with props passed from GamePage
    Reviewed by: Jason Ko, Jason Zhao, Aidan Dayvyd, Joel H
    
    * Issue: Resolve lint issue and general styles
    Solution: Resolve lint issue and general styles
    Risk: No Risk
    Reveiwed By: Dhwani, Jason, Aiden, Joel
    
    Co-authored-by: Jason Ko <jekb2019@gmail.com>

--- frontend/src/pages/resultsPage/resultsPage.jsx
+++ frontend/src/pages/resultsPage/resultsPage.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+/* eslint-disable */
+import React, { useState, useRef } from "react";
+import styles from "./resultsPage.module.css";
 
-const ResultsPage = ({ isPlayedBefore, gameResults }) => (
-  <div>
-    <h1>Results</h1>
-    <p>Played before: {isPlayedBefore}</p>
-    <p>Game Results: {JSON.stringify(gameResults)}</p>
-  </div>
-);
+const ResultsPage = (props) => {
+  return (
+    <section className={styles.whole_wrapper}>
+      <h1 className={styles.title}>Spot the Stick Insect</h1>
+      <div className={styles.upper_text_container}>
+        <p className={styles.thanks_message}>Thank you for participating!</p>
+        <p className={styles.results_message}>
+          You spotted <span className={styles.results_info}>18</span> stick
+          instects out of <span className={styles.results_info}>20</span>,
+          <br /> in an average time of{" "}
+          <span className={styles.results_info}>9.68 seconds</span>!
+        </p>
+      </div>
+
+      <p className={styles.email_instruction_message}>
+        If you would like to be notified of the outcome of this research, please
+        leave <br /> your email address below. If you choose to supply your
+        email address, <br />
+        we will not for any other purpose, nor will we supply it to any third
+        party.{" "}
+      </p>
+
+      <div className={styles.email_container}>
+        <label className={styles.email_label}> Email: </label>
+        <input className={styles.email_input} type="text" />
+        <button className={styles.email_btn}> SUBMIT</button>
+      </div>
+      <br />
+      <div className={styles.button_wrapper}>
+        <button className={styles.play_btn}> Try Again! </button>
+      </div>
+    </section>
+  );
+};
 
 export default ResultsPage;
+/* eslint-disable */
