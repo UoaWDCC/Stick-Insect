c0ef463cc24b48245928509d1c3e1267b80535a1
Jason Ko <jekb2019@gmail.com>
2022-03-06 22:24:16 +1300

    remove template agreement

--- frontend/src/pages/consentPage/consentPage.jsx
+++ frontend/src/pages/consentPage/consentPage.jsx
@@ -16,15 +16,7 @@ const ConsentPage = ({ switchToLandingPage, switchToInstructionPage }) => (
       <h1>Participant Information and Consent Form</h1>
     </header>
     <div className={styles.agreement_content}>
-      <p>
-        By clicking agree below... lorem ipsum dolor sit amet, consectetur
-        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-        qui officia deserunt mollit anim id est laborum
-      </p>
+      <p>By clicking agree below...</p>
     </div>
     <div className={styles.btns}>
       <ConsentButton func={switchToInstructionPage} text="I AGREE" />
